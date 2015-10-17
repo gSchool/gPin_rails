feb2fabb94862212ab93695cc7c835ea7d95bba3
Matt Lane <lane.matthew@gmail.com>
2015-10-16 22:21:34 -0700

    add new

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,4 +4,20 @@ class PinsController < ApplicationController
     @pins = Pin.all
   end
 
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.create(pin_params)
+    redirect_to '/pins'
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:url,:image,:comment)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   root 'pins#index'
   get '/pins', to: 'pins#index'
+  get '/pins/new', to: 'pins#new'
+  post '/pins', to: 'pins#create'
 
 end
