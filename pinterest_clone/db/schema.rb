6a221d0d70ab3dd13cb019bed2cbd53963cb5eeb
aaron <harms280@gmail.com>
2015-10-17 17:24:53 -0700

    created name column for Pin model

--- pinterest_clone/db/schema.rb
+++ pinterest_clone/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017225928) do
+ActiveRecord::Schema.define(version: 20151018002317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151017225928) do
     t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
