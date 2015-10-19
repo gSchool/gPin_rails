108683f3f22bbf707cf9fd9fb0a875973e3584cc
Matt Lane <lane.matthew@gmail.com>
2015-10-18 17:47:46 -0700

    refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
 
   root 'pins#index'
-  get '/pins', to: 'pins#index'
-  get '/pins/new', to: 'pins#new'
-  post '/pins', to: 'pins#create'
-  get '/pins/:id/', to: 'pins#show', as: :pin 
-  get '/pins/:id/edit', to: 'pins#edit'
-  put '/pins/:id', to: 'pins#update'
-  patch '/pins/:id', to: 'pins#update'
-  delete '/pins/:id', to: 'pins#destroy'
+  resources :pins
 
 end
