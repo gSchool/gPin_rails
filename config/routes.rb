446e9d32c7d2e070f95935039aec8f55ea8185b1
RoseTHERESA <rose.minta.theresa@gmail.com>
2015-10-19 18:47:41 -0700

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   root 'pins#index'
-  get'/pins', to: 'pins#index'
-  get '/pins/new', to: 'pins#new'
-  post '/pins', to: 'pins#create'
-  get '/pins/:id', to: 'pins#show', as: :pin
-  put '/pins/:id', to: 'pins#update'
-  patch '/pins/:id', to: 'pins#update'
-  get '/pins/:id/edit', to: 'pins#edit'
-  delete '/pins/:id', to: 'pins#destroy'
-end
+#   get'/pins', to: 'pins#index'
+#   get '/pins/new', to: 'pins#new'
+#   post '/pins', to: 'pins#create'
+#   get '/pins/:id', to: 'pins#show', as: :pin
+#   put '/pins/:id', to: 'pins#update'
+#   patch '/pins/:id', to: 'pins#update'
+#   get '/pins/:id/edit', to: 'pins#edit'
+#   delete '/pins/:id', to: 'pins#destroy'
+  resources :pins
+ end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
