c56d85cad98998f7c70ccd2a65656f30782a7820
Mark Marsella <mmarsella114@g.rwu.edu>
2015-10-19 19:24:20 -0700

    switched routes to resource :pins and update works

--- gPin_app/app/controllers/pins_controller.rb
+++ gPin_app/app/controllers/pins_controller.rb
@@ -14,9 +14,33 @@ class PinsController < ApplicationController
     redirect_to "/pins"
   end
 
+  def show
+    # is the params method from the 
+    # url params from the routes??
+    @pin = Pin.find(params[:id])
+  end
+
+  def edit
+    @pin = Pin.find(params[:id])
+  end
+
+  def update
+    @pin = Pin.find(params[:id])
+
+    # What is going on HERE??
+    @pin.update(pin_params) 
+    redirect_to "/pins"
+
+  end
+
+  def destroy
+  end
 
 
 
+
+
+  # Why do we do THIS?
   private
   def pin_params
     params.require(:pin).permit(:url,:comment,:image)
