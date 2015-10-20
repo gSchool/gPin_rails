769d32b96007f920694641ec3999d2caf50025c4
Mark Marsella <mmarsella114@g.rwu.edu>
2015-10-19 18:08:45 -0700

    add index styling, add addPin

--- gPin_app/app/controllers/pins_controller.rb
+++ gPin_app/app/controllers/pins_controller.rb
@@ -1,6 +1,26 @@
 class PinsController < ApplicationController
+  
   def index
     @pins = Pin.all
     render :index
   end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    Pin.create pin_params
+    redirect_to "/pins"
+  end
+
+
+
+
+  private
+  def pin_params
+    params.require(:pin).permit(:url,:comment,:image)
+  end
+
+
 end
